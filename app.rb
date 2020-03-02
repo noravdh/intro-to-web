5393e143cf89890af82574d029491f56ea442fd4
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-02 15:43:07 +0000

    params

--- app.rb
+++ app.rb
@@ -9,7 +9,13 @@ get '/secret' do
   "This is a secret"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample 
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
   erb(:index)
 end
