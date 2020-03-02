a6c8bd1c12f06b97f44cd0a7155e7142068a3e40
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-02 15:00:45 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+
+
+source "https://rubygems.org"
+
+gem 'sinatra'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+
+
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "This is a secret"
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
